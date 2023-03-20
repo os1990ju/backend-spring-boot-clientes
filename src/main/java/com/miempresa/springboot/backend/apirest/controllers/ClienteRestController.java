6ce3747a1e966513be6606afc9239b7e8a268365
oscar.jurado <oscar.jurado@arkangroupco.com>
2023-03-20 01:14:10 -0500

    vaidacion de formulario

--- src/main/java/com/miempresa/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/miempresa/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -28,8 +31,8 @@ public class ClienteRestController {
     @GetMapping("/clientes/{id}")
     public ResponseEntity<?> show(@PathVariable Long id){
 
-        Cliente cliente = null;
-        Map<String,Object> response = new HashMap<String,Object>();
+        Cliente cliente;
+        Map<String,Object> response = new HashMap<>();
         try{
             cliente = clienteService.findById(id);
         }catch(DataAccessException e){
@@ -45,10 +48,20 @@ public class ClienteRestController {
     }
     //crear cliente
     @PostMapping("/clientes")
-    public ResponseEntity<?> create(@RequestBody Cliente cliente){
+    public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result){
 //        intenta guradar
         Cliente clienteNew = null;
         Map<String,Object> response = new HashMap<>();
+
+        //manejo de error de reuqest body
+        if(result.hasErrors()){
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(err -> "El campo'"+err.getField()+"'"+err.getDefaultMessage())
+                    .collect(Collectors.toList());
+            response.put("errors", errors);
+            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.BAD_REQUEST);
+        }
         try{
             clienteNew = clienteService.save(cliente);
         }catch(DataAccessException e){
@@ -62,7 +75,7 @@ public class ClienteRestController {
     }
     //actualizar
     @PutMapping("/clientes/{id}")
-    public  ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id){
+    public  ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result , @PathVariable Long id){
 
         Cliente clienteActual = clienteService.findById(id);
         Cliente clienteUpdated = null;
@@ -71,6 +84,15 @@ public class ClienteRestController {
         if(clienteActual == null){
             response.put("mensaje", "Error: no se puede editar, el cliente ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
             return new ResponseEntity<Map<String,Object>>(response,HttpStatus.NOT_FOUND);
+        }
+        //manejo de error de reuqest body
+        if(result.hasErrors()){
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(err -> "El campo'"+err.getField()+"'"+err.getDefaultMessage())
+                    .collect(Collectors.toList());
+            response.put("errors", errors);
+            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.BAD_REQUEST);
         }
         try {
 
@@ -97,7 +119,7 @@ public class ClienteRestController {
         } catch(DataAccessException e){
             response.put(KeyConstants.MESSAGE , KeyConstants.ERROR_DELETED );
             response.put(KeyConstants.ERROR , e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         response.put(KeyConstants.MESSAGE , KeyConstants.CLIENT_DELETE_SUCCESS);
         return new ResponseEntity<Map<String,Object>>(response,HttpStatus.NO_CONTENT);
--- src/main/java/com/miempresa/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/miempresa/springboot/backend/apirest/models/entity/Cliente.java
@@ -1,6 +1,9 @@
 package com.miempresa.springboot.backend.apirest.models.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -14,11 +17,16 @@ public class Cliente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 50, nullable = false)
+    @NotEmpty(message="No puede estar vacio")
+    @Size(min=4, max=12, message="El tamaño tiene que estar entre 4 y 12 caracteres")
+
+    @Column(nullable = false)
     private String nombre;
-    @Column(nullable =false)
+    @NotEmpty(message="No puede estar vacio")
     private String apellido;
-    @Column(length = 50, unique=true, nullable = false)
+    @NotEmpty(message="No puede estar vacio")
+    @Email(message="debe ser una dirección de email válida")
+    @Column(nullable = false, unique=true)
     private String email;
     @Column(name="create_at")
     @Temporal(TemporalType.DATE) //transforma la fecha dejava a date en sql
